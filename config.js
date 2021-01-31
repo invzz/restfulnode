462394c78e2fdd515e4ccc8ed2a8e440ac499952
Andres Coronado <invizuz@gmail.com>
2021-01-31 15:57:32 +0100

    checks

--- config.js
+++ config.js
@@ -1,41 +1,41 @@
+/* eslint-disable no-console */
 /* config */
 
-var environments = {}
+const environments = {};
 
-// dev 
+// dev
 environments.dev = {
-    'httpPort': 3000,
-    'httpsPort': 3001,
-    'hashingSecret': 'secret',
-    'envName': 'dev'
+  httpPort: 3000,
+  httpsPort: 3001,
+  hashingSecret: 'secret',
+  envName: 'dev',
+  maxChecks: 5,
 };
 
 // prodution
 environments.prod = {
-    'httpPort': 8080,
-    'httpsPort': 8081,
-    'hashingSecret': 'secret',
-    'envName': 'prod'
+  httpPort: 8080,
+  httpsPort: 8081,
+  hashingSecret: 'secret',
+  envName: 'prod',
+  maxChecks: 5,
 };
 
 // if not specified, conffig will fallback to defaul env
 environments.default = environments.dev;
 
-
 // gets the environment if exist.
-getEnv = () => {
-    var selectedEnv = typeof(process.env.NODE_ENV === 'string') ? process.env.NODE_ENV : '';
-    if (typeof(environments[selectedEnv]) == 'object') {
-        return environments[selectedEnv];
-    } else {
-        console.log("[ Config ] env", process.env.NODE_ENV, " not found. fallback to default ");
-        return environments.default;
-    }
-}
-
+const getEnv = () => {
+  const selectedEnv = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : '';
+  if (typeof (environments[selectedEnv]) === 'object') {
+    return environments[selectedEnv];
+  }
+  console.log('[ Config ] env', process.env.NODE_ENV, ' not found. fallback to default ');
+  return environments.default;
+};
 
 // getting env to be set
-var env = getEnv();
+const env = getEnv();
 
 // logs current environment in use
 console.log('[ Config ] env : ', env.envName);
--- index.js
+++ index.js
@@ -1,101 +1,97 @@
+/* eslint-disable no-console */
 /* entry point */
 
 // dependencies
 
 // http protocol and server
-var http = require('http');
+const http = require('http');
 
 // https protocol and server
-var https = require('https');
-
+const https = require('https');
 
 // file system to access files
-var fs = require('fs');
+const fs = require('fs');
 
 // decodes strings using (UTF-8)
-var StringDecoder = require('string_decoder').StringDecoder;
+const { StringDecoder } = require('string_decoder');
 
 // environments
-var env = require('./config.js');
+const env = require('./config.js');
 
 // routes
-var routes = require('./routes');
+const routes = require('./routes');
 
 // helper functions
-var helpers = require('./lib/helpers')
+const helpers = require('./lib/helpers');
 
+// All server logic for both http and https
+const unifiedServer = (request, response) => {
+  // decodes data to utf8
+  const decoder = new StringDecoder('utf-8');
 
+  // buffer to store data stream
+  let buffer = '';
 
-// http server definition 
-const httpServer = http.createServer(
-    function(request, response) {
-        unifiedServer(request, response);
+  // open stream and decode data when as it comes to the server
+  request.on('data', (data) => { buffer += decoder.write(data); });
+
+  // when data has been receiveid
+  request.on('end', () => {
+    // end buffer string which holds the reques body
+    buffer += decoder.end();
+
+    // getting useful object to work with from request
+    const data = helpers.reqToObj(request, buffer);
+
+    // log request
+    console.log('[ request ] ', data);
+
+    // routing to the right controller
+    const controller = routes[data.route] ? routes[data.route] : routes.notfound;
+
+    // executing controller
+    controller(data, (HTTPstatus, result) => {
+      const status = HTTPstatus || 200;
+
+      // default result empty object
+      const resultObj = typeof (result) === 'object' ? result : {};
+
+      // Content type
+      response.setHeader('Content-Type', 'application/json');
+
+      // Response status
+      response.writeHead(status);
+
+      // send response
+      response.end(JSON.stringify(resultObj));
+
+      // log response
+      console.log('[ response ] ', resultObj);
     });
+  });
+};
+
+// http server definition
+const httpServer = http.createServer(
+  (request, response) => {
+    unifiedServer(request, response);
+  },
+);
 
 // https Server definition
-var httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem'),
+const httpsServerOptions = {
+  key: fs.readFileSync('./https/key.pem'),
+  cert: fs.readFileSync('./https/cert.pem'),
 };
 
 // create the https server
 const httpsServer = https.createServer(
-    httpsServerOptions,
-    (request, response) => unifiedServer(request, response)
+  httpsServerOptions,
+  (request, response) => unifiedServer(request, response),
 );
 
-//starting http server
-httpServer.listen(env.httpPort, () => console.log("[ HTTP  ] Listening on port : " + env.httpPort));
+// starting http server
+httpServer.listen(env.httpPort, () => console.log(`[ HTTP  ] Listening on port : ${env.httpPort}`));
 
-//starting http server
-httpsServer.listen(env.httpsPort, () => console.log("[ HTTPS ] Listening on port : " + env.httpsPort));
-
-
-// All server logic for both http and https
-var unifiedServer = function(request, response) {
-    // decodes data to utf8
-    const decoder = new StringDecoder('utf-8');
-
-    // buffer to store data stream
-    var buffer = '';
-
-    // open stream and decode data when as it comes to the server
-    request.on('data', (data) => buffer += decoder.write(data));
-
-    // when data has been receiveid 
-    request.on('end', () => {
-
-        // end buffer string which holds the reques body 
-        buffer += decoder.end();
-
-        // getting useful object to work with from request
-        var data = helpers.reqToObj(request, buffer);
-
-        // log request
-        console.log('[ request ] ', data);
-
-        // routing to the right controller
-        var controller = !!routes[data.route] ? routes[data.route] : routes['notfound'];
-
-        // executing controller
-        controller(data, (status, result) => {
-            // default status 200
-            status = !!status ? status : 200;
-
-            // default result empty object
-            result = typeof(result) === 'object' ? result : {};
-
-            // Content type
-            response.setHeader('Content-Type', 'application/json');
-
-            // Response status 
-            response.writeHead(status);
-
-            // send response
-            response.end(JSON.stringify(result));
-
-            // log response
-            console.log('[ response ] ', result);
-        });
-    });
-};
+// starting http server
+httpsServer.listen(env.httpsPort, () => console.log(`[ HTTPS ] Listening on port : ${env.httpsPort}`));
